f5132cac7c1a11b01e55b4f3b1d40ca8da514593
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-08-01 23:05:25 +0500

    up

---
+++ js.js
@@ -0,0 +1,19 @@
+const deg = 6;
+
+// получение элементов
+let hr = document.querySelector('#hr');
+let mn = document.querySelector('#mn');
+let sc = document.querySelector('#sc');
+
+// каждую секунду вызываем функцию
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+},1000);
